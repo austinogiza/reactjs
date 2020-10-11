0d7276cdb81cfe2cf9b749c3362a746c62a25f89
Augustine Ogiza <austinogiza@gmail.com>
2020-10-11 14:44:35 +0100

    build

---
+++ build/precache-manifest.34fba84f3392bd58b4ca3db762e0fc15.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f60743cdc7acc1179f7c7a91015ed87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1814af8ce4ea54ff70",
+    "url": "/static/css/main.d80dacfa.chunk.css"
+  },
+  {
+    "revision": "e4bfefe2634386f8db47",
+    "url": "/static/js/2.a6e79414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6e79414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1814af8ce4ea54ff70",
+    "url": "/static/js/main.9eae45ff.chunk.js"
+  },
+  {
+    "revision": "18710acd3638a8ae2a1e",
+    "url": "/static/js/runtime-main.361cba25.js"
+  },
+  {
+    "revision": "4e40789fe985a2ba34cdf76c7fa5a54e",
+    "url": "/static/media/EuclidCircularB-Bold.4e40789f.ttf"
+  },
+  {
+    "revision": "a523ad473d5bad00dcdda4a3a46cec5d",
+    "url": "/static/media/EuclidCircularB-Light.a523ad47.ttf"
+  },
+  {
+    "revision": "e5a8e44f6ed4560fa934c8e0233d1597",
+    "url": "/static/media/EuclidCircularB-Medium.e5a8e44f.ttf"
+  },
+  {
+    "revision": "f29cfc3fa5ade3421e1a2c66f9e01a34",
+    "url": "/static/media/EuclidCircularB-Regular.f29cfc3f.ttf"
+  },
+  {
+    "revision": "c127460819cb4c19f2715339c9387ba8",
+    "url": "/static/media/bg.c1274608.png"
+  },
+  {
+    "revision": "f9233770130d9fef3f9a5ab7dd01a134",
+    "url": "/static/media/logo-figma.f9233770.svg"
+  },
+  {
+    "revision": "29a4244962d4510043b5f315ecefea70",
+    "url": "/static/media/logo-framer.29a42449.svg"
+  },
+  {
+    "revision": "8b285cc0a4c44a1e36920f04501fd71f",
+    "url": "/static/media/logo-invision.8b285cc0.svg"
+  },
+  {
+    "revision": "d60d8202a1d71131331a2fe927e162e1",
+    "url": "/static/media/logo-react.d60d8202.svg"
+  },
+  {
+    "revision": "5ce493ec0da1c94cd45f8c3639fc9d02",
+    "url": "/static/media/logo-sketch.5ce493ec.svg"
+  },
+  {
+    "revision": "b957b2d87280352e7942ad2ba8db999b",
+    "url": "/static/media/logo-xcode.b957b2d8.svg"
+  },
+  {
+    "revision": "9202cd024162da5c053daa886d9fd152",
+    "url": "/static/media/logo.9202cd02.svg"
+  }
+]);
